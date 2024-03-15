58f2b39bed061d0f3c74a241d0e6f6e7e32eba48
start.vaadin.com <no-reply@vaadin.com>
2024-03-15 13:25:31 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/myview/MyViewView.java
@@ -0,0 +1,41 @@
+package com.example.application.views.myview;
+
+import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.dependency.Uses;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
+
+@PageTitle("My View")
+@Route(value = "my-view", layout = MainLayout.class)
+@RouteAlias(value = "", layout = MainLayout.class)
+@Uses(Icon.class)
+public class MyViewView extends Composite<VerticalLayout> {
+
+    public MyViewView() {
+        HorizontalLayout layoutRow = new HorizontalLayout();
+        VerticalLayout layoutColumn2 = new VerticalLayout();
+        VerticalLayout layoutColumn3 = new VerticalLayout();
+        HorizontalLayout layoutRow2 = new HorizontalLayout();
+        getContent().setWidth("100%");
+        getContent().getStyle().set("flex-grow", "1");
+        layoutRow.addClassName(Gap.MEDIUM);
+        layoutRow.setWidth("100%");
+        layoutRow.getStyle().set("flex-grow", "1");
+        layoutColumn2.getStyle().set("flex-grow", "1");
+        layoutColumn3.setWidth("100%");
+        layoutColumn3.getStyle().set("flex-grow", "1");
+        layoutRow2.addClassName(Gap.MEDIUM);
+        layoutRow2.setWidth("100%");
+        layoutRow2.setHeight("min-content");
+        getContent().add(layoutRow);
+        layoutRow.add(layoutColumn2);
+        layoutRow.add(layoutColumn3);
+        getContent().add(layoutRow2);
+    }
+}
